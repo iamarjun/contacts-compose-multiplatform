e4edba2d377ac6fbfc8d08ac99c3e2bd8a271ff4
iamarjun <arjunmanoj1995@gmail.com>
2023-08-20 01:12:56 +0530

    enable dark mode in iOS

--- shared/src/iosMain/kotlin/com/plcoding/contactscomposemultiplatform/main.ios.kt
+++ shared/src/iosMain/kotlin/com/plcoding/contactscomposemultiplatform/main.ios.kt
@@ -1,5 +1,12 @@
 package com.plcoding.contactscomposemultiplatform
 
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIScreen
+import platform.UIKit.UIUserInterfaceStyle
 
-fun MainViewController() = ComposeUIViewController { App(darkTheme = false, dynamicColors = true) }
+fun MainViewController() = ComposeUIViewController {
+    App(
+        darkTheme = UIScreen.mainScreen.traitCollection.userInterfaceStyle == UIUserInterfaceStyle.UIUserInterfaceStyleDark,
+        dynamicColors = true
+    )
+}
